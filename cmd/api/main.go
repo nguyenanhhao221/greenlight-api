de81e36efe9d617ab70673fd8dbad14d85523980
Hao Nguyen <hao@haonguyen.tech>
2025-05-10 16:49:44 +0700

    refactor: update default port to 42069

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -25,7 +25,7 @@ func main() {
 	var cfg config
 
 	// Get server config via cli flag
-	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.IntVar(&cfg.port, "port", 42069, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.Parse()
 
