7a17cf1e9e482d1ca7a48db26b1b3073462831d4
Hao Nguyen <hao@haonguyen.tech>
2025-05-10 16:57:10 +0700

    feat: add basic handler for movies

---
+++ cmd/api/movies.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	if _, err := fmt.Fprintln(w, "create a new movie"); err != nil {
+		log.Printf("error writing to http ResponseWriter: %v\n", err)
+	}
+}
+
+func (app *application) showMovieHanlder(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	if _, err := fmt.Fprintf(w, "show the details of movies %d\n", id); err != nil {
+		log.Printf("error writing to http ResponseWriter: %v\n", err)
+	}
+}
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,6 +9,11 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	// Map the routes
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
+
+	// Movies routes
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHanlder)
 	return router
 }
