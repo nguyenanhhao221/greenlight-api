566c8d61603abc7cfdd6b9f8f973e483bc78746a
Hao Nguyen <hao@haonguyen.tech>
2025-05-10 17:11:13 +0700

    feat(json): use simple json response in healthCheckHandler

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status:available")
-	fmt.Fprintf(w, "environment:%s\n", app.config.env)
-	fmt.Fprintf(w, "version:%s\n", version)
+	js := `{"status": "available" , "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if _, err := w.Write([]byte(js)); err != nil {
+		log.Printf("error wring to http.ResponseWriter: %v\n", err)
+		return
+	}
 }
