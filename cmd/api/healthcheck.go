6cb193aaf053214acc72392c1f8d709c4c505c68
Hao Nguyen <hao@haonguyen.tech>
2025-05-12 01:35:40 +0700

    feat(api): use envelope for healthcheck response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,13 +5,13 @@ import (
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := map[string]string{
+	env := envelop{
 		"status":      "available",
 		"environment": app.config.env,
 		"version":     version,
 	}
 
-	if err := app.writeJSON(w, http.StatusOK, js, nil); err != nil {
+	if err := app.writeJSON(w, http.StatusOK, env, nil); err != nil {
 		app.logger.Printf("error writing to JSON: %v\n", err)
 		http.Error(w, "The server encounter a problem and couldn't process your request", http.StatusInternalServerError)
 	}
