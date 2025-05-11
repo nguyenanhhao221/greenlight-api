2de41a30777975c22f3017ba023e500706f5c64c
Hao Nguyen <hao@haonguyen.tech>
2025-05-12 01:45:17 +0700

    feat(json): add custom Runtime time to try out custom json marshaler

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -7,7 +7,7 @@ type Movie struct {
 	CreatedAt time.Time `json:"-"`                 // Time stamp for when the movie is added to our database
 	Title     string    `json:"title"`             // Movie title
 	Year      int32     `json:"year,omitempty"`    // Movie release year
-	Runtime   int32     `json:"runtime,omitempty"` // Movie run time (in minutes)
+	Runtime   Runtime   `json:"runtime,omitempty"` // Movie run time (in minutes)
 	Genres    []string  `json:"genres,omitempty"`  // Slice of genres for the movie (romance, comedy, etc.)
 	Version   int32     `json:"version"`           // The version number starts at 1 and will be incremented each time the movie information is updated
 }
