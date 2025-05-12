ec2b355f3e5b8bf39da6020b18829096aeed0215
Hao Nguyen <hao@haonguyen.tech>
2025-05-12 23:58:55 +0700

    feat: add basic create movie data type response

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,6 +13,24 @@ import (
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	if _, err := fmt.Fprintln(w, "create a new movie"); err != nil {
 		log.Printf("error writing to http ResponseWriter: %v\n", err)
+	}
+
+	var movieInputData struct {
+		Title   string   `json:"title"`   // Movie title
+		Year    int32    `json:"year"`    // Movie release year
+		Runtime int32    `json:"runtime"` // Movie run time (in minutes)
+		Genres  []string `json:"genres"`  // Slice of genres for the movie (romance, comedy, etc.)
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&movieInputData)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	_, err = fmt.Fprintf(w, "%+v\n", movieInputData)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
 	}
 }
 
