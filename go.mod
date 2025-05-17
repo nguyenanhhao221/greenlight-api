e7628f300eb91ea8f6d8076d2d46713d52204cb1
Hao Nguyen <hao@haonguyen.tech>
2025-05-17 17:06:02 +0700

    feat: remove pq library

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.24.0
 require (
 	github.com/jackc/pgx/v5 v5.7.4
 	github.com/julienschmidt/httprouter v1.3.0
-	github.com/lib/pq v1.10.9
 )
 
 require (
--- internal/models/movie_models.go
+++ internal/models/movie_models.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/lib/pq"
 	"github.com/nguyenanhhao221/greenlight-api/internal/data"
 )
 
@@ -18,7 +17,7 @@ func (m *MovieModel) Create(movie *data.Movie) error {
 		VALUES ($1, $2, $3, $4)
 		RETURNING id, created_at, version;
 	`
-	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+	args := []any{movie.Title, movie.Year, movie.Runtime, movie.Genres}
 
 	return m.DB.QueryRow(context.Background(), query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
