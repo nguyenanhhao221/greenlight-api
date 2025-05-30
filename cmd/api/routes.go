f58fdc93336e22c883ddb0b8b81bd4755d616646
Hao Nguyen <hao@haonguyen.tech>
2025-05-30 23:36:20 +0700

    feat(middleware): add panic recovery middleware for improved error handling
    - Introduced a new middleware function, recoverPanic, to handle panics gracefully in HTTP handlers.
    - The middleware captures panic errors, sets the appropriate response headers, and invokes the server error response method.
    - Updated the routes function to wrap the router with the new recoverPanic middleware for enhanced stability.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -23,5 +23,5 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHanlder)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
-	return router
+	return app.recoverPanic(router)
 }
