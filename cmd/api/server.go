6d0770e85ce6eaf3d8624d60c2016b3f87281548
Hao Nguyen <hao@haonguyen.tech>
2025-05-31 18:44:07 +0700

    feat(server): implement graceful shutdown for HTTP server
    - Added support for graceful shutdown of the server using context and signal handling.
    - Introduced a shutdownError channel to manage server shutdown errors.
    - Enhanced logging to indicate server start and stop events, including the shutdown signal received.
    - Ensured that the server properly handles shutdown errors after ListenAndServe.

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -17,7 +21,29 @@ func (app *application) serve() error {
 		WriteTimeout: 30 * time.Second,
 		ErrorLog:     slog.NewLogLogger(slog.NewTextHandler(os.Stderr, nil), slog.LevelError),
 	}
+	shutdownError := make(chan error)
+
+	// Graceful shutdown
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+		app.logger.Info("Shutting down server", "signal", s.String())
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		shutdownError <- srv.Shutdown(ctx)
+	}()
 
 	app.logger.Info("Starting server", "Address", srv.Addr, "environment", app.config.env, "limiter", app.config.limiter)
-	return srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+	app.logger.Info("Stopped server", "Address", srv.Addr, "environment", app.config.env)
+	return nil
 }
